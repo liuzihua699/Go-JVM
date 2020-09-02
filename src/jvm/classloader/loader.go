b97440f08ff069737c792d09f76a14a3e8c4307e
刘子华 <2274298503@qq.com>
2020-09-02 18:18:02 +0800

    update based-loader detail.

--- src/jvm/classloader/loader.go
+++ src/jvm/classloader/loader.go
@@ -5,7 +5,9 @@ import (
 	"strings"
 )
 
-const SEPARATOR = string(os.PathListSeparator)
+const List_SEPARATOR = string(os.PathListSeparator)
+const SEPARATOR = string(os.PathSeparator)
+const CLASS_SEPARATOR = "/"
 
 /**
   From 《Java Virtual Machine Specification》, I implement 4 type class-loader,
@@ -21,20 +23,21 @@ type ClassLoader interface {
 }
 
 /**
-  create entry from path
-  excute createCompositeLoader when path has recursion catalog. e.g com/zihua/User
-  excute createWildcardLoader when path has wildcard. e.g com/* or *
-  excute createZipLoader when path has .jar or .zip file
-  else excute createDirLoader
+  create loader from path
+  excute CreateCompositeLoader when path has recursion catalog. e.g com/zihua/User
+  excute CreateWildcardLoader when path has wildcard. e.g com/* or *
+  excute CreateZipLoader when path has .jar or .zip file
+  else excute CreateDirLoader
 */
 func CreateLoader(path string) ClassLoader {
-	if strings.Contains(path, SEPARATOR) {
+	if strings.Contains(path, List_SEPARATOR) {
 		return CreateCompositeLoader(path)
 	}
 	if strings.HasSuffix(path, "*") {
 		return CreateWildcardLoader(path)
 	}
-	if strings.HasSuffix(path, ".jar") || strings.HasSuffix(path, ".JAR") {
+	if strings.HasSuffix(path, ".jar") || strings.HasSuffix(path, ".JAR") ||
+		strings.HasSuffix(path, ".zip") || strings.HasSuffix(path, ".ZIP") {
 		return CreateZipLoader(path)
 	}
 	return CreateDirLoader(path)
