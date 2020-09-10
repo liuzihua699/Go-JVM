b6d3a62366ab1f0b5b9b66c210caba634fcad18b
刘子华 <2274298503@qq.com>
2020-09-10 11:29:59 +0800

    added CLASS_FILE format defined in Oracle official.

---
+++ src/jvm/class/class_file.go
@@ -0,0 +1,52 @@
+package class
+
+import "jvm/class/constant_pool"
+
+/*
+ClassFile {
+    u4             magic;
+    u2             minor_version;
+    u2             major_version;
+    u2             constant_pool_count;
+    cp_info        constant_pool[constant_pool_count-1];
+    u2             access_flags;
+    u2             this_class;
+    u2             super_class;
+    u2             interfaces_count;
+    u2             interfaces[interfaces_count];
+    u2             fields_count;
+    field_info     fields[fields_count];
+    u2             methods_count;
+    method_info    methods[methods_count];
+    u2             attributes_count;
+    attribute_info attributes[attributes_count];
+}
+*/
+
+type ClassFile struct {
+	magic             uint32
+	minjorVersion     uint16
+	majorVersion      uint16
+	constantPoolCount uint16
+	constantPool      constant_pool.ConstantPool
+	accessFlags       uint16
+	thisClass         uint16
+	superClass        uint16
+	interfacesCount   uint16
+	interfaces        []uint16
+	fieldsCount       uint16
+	//fields            []*MemberInfo
+	methodsCount uint16
+	//methods           []*MemberInfo
+	attributesCount uint16
+	//attributes 			[]*AttributeInfo
+}
+
+// 字节码校验
+/*func (c ClassFile) check() bool {
+	return true
+}*/
+
+/*func (c ClassFile) Parse(byteStream []byte) (*ClassFile, error) {
+
+}*/
