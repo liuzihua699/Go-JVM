9db3dec85d06d64eaea00b13f8f27da943c9cbbd
刘子华 <2274298503@qq.com>
2020-09-14 14:50:20 +0800

    add some annotation and notes.

--- src/jvm/class/attribute/attr_info_Code.go
+++ src/jvm/class/attribute/attr_info_Code.go
@@ -4,6 +4,24 @@ import (
 	"jvm/class/class_file_commons"
 )
 
+/**
+Code_attribute {
+    u2 attribute_name_index;
+    u4 attribute_length;
+    u2 max_stack;
+    u2 max_locals;
+    u4 code_length;
+    u1 code[code_length];
+    u2 exception_table_length;
+    {   u2 start_pc;
+        u2 end_pc;
+        u2 handler_pc;
+        u2 catch_type;
+    } exception_table[exception_table_length];
+    u2 attributes_count;
+    attribute_info attributes[attributes_count];
+}
+*/
 type Code_attribute struct {
 	AttributeNameIndex   uint16
 	AttributeLength      uint32
@@ -24,6 +42,14 @@ type ExceptionTable struct {
 	CatchType uint16
 }
 
+func (e *ExceptionTable) readExceptionTable(reader class_file_commons.Reader) ExceptionTable {
+	e.StartPc = reader.ReadUint16()
+	e.EndPc = reader.ReadUint16()
+	e.HandlerPc = reader.ReadUint16()
+	e.CatchType = reader.ReadUint16()
+	return *e
+}
+
 func (c *Code_attribute) ReadAttrInfo(reader class_file_commons.Reader) AttrInfo {
 	c.MaxStrack = reader.ReadUint16()
 	c.MaxLocals = reader.ReadUint16()
@@ -32,12 +58,7 @@ func (c *Code_attribute) ReadAttrInfo(reader class_file_commons.Reader) AttrInfo
 	c.ExceptionTableLength = reader.ReadUint16()
 	size := c.ExceptionTableLength
 	for i := 0; i < int(size); i++ {
-		ex := new(ExceptionTable)
-		ex.StartPc = reader.ReadUint16()
-		ex.EndPc = reader.ReadUint16()
-		ex.HandlerPc = reader.ReadUint16()
-		ex.CatchType = reader.ReadUint16()
-		c.ExceptionTable = append(c.ExceptionTable, *ex)
+		c.ExceptionTable = append(c.ExceptionTable, new(ExceptionTable).readExceptionTable(reader))
 	}
 	ar := AttributesStructReader{reader}
 	a_count, a_infos := ar.ReadAttributeInfos()
